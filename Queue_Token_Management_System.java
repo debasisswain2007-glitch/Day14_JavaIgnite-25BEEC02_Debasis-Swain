e6397d1834823e9e1e3d393eb5fc26298f967dd8
rite2harini <rite2harini@gmail.com>
2026-06-10 15:08:38 +0530

    Create Queue Token Management System
    Implement a token management system for hospital patients using a queue.

---
+++ Queue_Token_Management_System.java
@@ -0,0 +1,17 @@
+/*
+Token Management System
+
+A hospital issues tokens to patients.
+
+Requirements:
+Add 5 patient names into a Queue.
+Display all patients waiting in the queue.
+Serve the first patient.
+Display the patient being served.
+Display the remaining queue.
+Concepts Tested:
+Queue
+offer()
+poll()
+peek()
+*/
